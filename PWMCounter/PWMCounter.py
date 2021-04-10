45877a4a9867cffd95e899b84308ab4d91cc78b2
Horu <horuable@gmail.com>
2021-04-10 16:50:19 +0200

    General code cleanup.

--- PWMCounter/PWMCounter.py
+++ PWMCounter/PWMCounter.py
@@ -4,35 +4,33 @@ _conditions = {"FREE" : 0x0, "GATED" : 0x1, "EDGE_RISING" : 0x2, "EDGE_FALLING" 
 
 class PWMCounter:
     def __init__(self, pin, condition = "FREE"):
-        assert pin < 30
-        # Only odd pins can be counter inputs
-        assert pin % 2
-        self.pin_reg = 0x40014000 | (0x04 + pin * 8)
-        self.csr = 0x40050000 | (0x00 + (pin % 16) // 2 * 20)
-        self.ctr = 0x40050000 | (0x08 + (pin % 16) // 2 * 20)
-        self.div = 0x40050000 | (0x04 + (pin % 16) // 2 * 20)
-#         print(hex(self.pin_reg), hex(self.csr), hex(self.ctr))
-        self.condition = condition
+        assert pin < 30 and pin % 2, "Invalid pin number"
+        slice_offset = (pin % 16) // 2 * 20
+        self._pin_reg = 0x40014000 | (0x04 + pin * 8)
+        self._csr = 0x40050000 | (0x00 + slice_offset)
+        self._ctr = 0x40050000 | (0x08 + slice_offset)
+        self._div = 0x40050000 | (0x04 + slice_offset)
+        self._condition = condition
         self.setup()
     
     def setup(self):
         # Set pin to PWM
-        mem32[self.pin_reg] = 4
+        mem32[self._pin_reg] = 4
         # Setup PWM counter for selected pin to chosen counter mode
-        mem32[self.csr] = _conditions[self.condition] << 4
+        mem32[self._csr] = _conditions[self._condition] << 4
         self.reset()
     
     def start(self):
-        mem32[self.csr + 0x2000] = 1
+        mem32[self._csr + 0x2000] = 1
         
     def stop(self):
-        mem32[self.csr + 0x3000] = 1
+        mem32[self._csr + 0x3000] = 1
     
     def reset(self):
-        mem32[self.ctr] = 0
+        mem32[self._ctr] = 0
     
     def read(self):
-        return mem32[self.ctr]
+        return mem32[self._ctr]
     
     def read_and_reset(self):
         tmp = self.read()
@@ -47,10 +45,8 @@ if __name__ == "__main__":
     from machine import Pin
     output = Pin(2, Pin.OUT)
     counter = PWMCounter(15, "EDGE_RISING")
-#     counter._conditions["EDGE_RISING"] = 5
-#     print(counter._conditions)
-#     counter.start()
-#     for i in range(5):
-#         output.value(1)
-#         output.value(0)
-#         print(counter.read())
+    counter.start()
+    for i in range(5):
+        output.value(1)
+        output.value(0)
+        print(counter.read())
